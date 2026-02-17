a9a2008ce2c3a975b8c40b27a96088af4de8e49d
Roberto <r.acebes.2024@alumnos.urjc.es>
2026-02-17 10:28:21 +0100

    changes

---
+++ src/main/java/es/codeurjc/ferrumgym/model/Activity.java
@@ -0,0 +1,72 @@
+package es.codeurjc.ferrumgym.model;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Activity {
+    
+    public Activity(Long id, String name, String description, byte[] image, List<Booking> bookings) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.bookings = bookings;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    
+    @Column(columnDefinition = "TEXT")
+    private String description; // Will be Rich Text in Practice 3 [cite: 239, 554]
+
+    @Lob
+    private byte[] image; // Activity photo [cite: 238, 552, 657]
+
+    @OneToMany(mappedBy = "activity", cascade = CascadeType.ALL)
+    private List<Booking> bookings;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    
+}
---
+++ src/main/java/es/codeurjc/ferrumgym/model/Booking.java
@@ -0,0 +1,62 @@
+package es.codeurjc.ferrumgym.model;
+import jakarta.persistence.*;
+import java.util.List;
+import java.time.LocalDateTime;
+
+@Entity
+public class Booking {
+    
+    public Booking(Long id, User user, Activity activity, LocalDateTime bookingDate) {
+        this.id = id;
+        this.user = user;
+        this.activity = activity;
+        this.bookingDate = bookingDate;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User user; // The owner of the booking 
+
+    @ManyToOne
+    private Activity activity;
+
+    private LocalDateTime bookingDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public LocalDateTime getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDateTime bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    
+    
+}
