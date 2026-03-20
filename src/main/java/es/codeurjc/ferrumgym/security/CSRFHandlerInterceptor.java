7dfaf0d7623ee6c6f439406404eba585b7e1517f
Elena <e.gonzalezro.2024@alumnos.urjc.es>
2026-03-20 11:46:00 +0100

    Add CSRFHandlerInterceptor for global CSRF token injection in views

---
+++ src/main/java/es/codeurjc/ferrumgym/security/CSRFHandlerInterceptor.java
@@ -0,0 +1,19 @@
+package es.codeurjc.ferrumgym.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+
+@ControllerAdvice
+public class CSRFHandlerInterceptor {
+
+    @ModelAttribute
+    public void addCsrfToModel(Model model, HttpServletRequest request) {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        if (csrfToken != null) {
+            model.addAttribute("_csrf", csrfToken);
+        }
+    }
+}
