9c9f44157756700619e40a7415d8d1d53b139203
Elena <e.gonzalezro.2024@alumnos.urjc.es>
2026-03-24 10:27:33 +0100

    changes

--- src/main/java/es/codeurjc/ferrumgym/controller/AdminController.java
+++ src/main/java/es/codeurjc/ferrumgym/controller/AdminController.java
@@ -11,21 +11,13 @@ import es.codeurjc.ferrumgym.model.User;
 import es.codeurjc.ferrumgym.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.util.List;
