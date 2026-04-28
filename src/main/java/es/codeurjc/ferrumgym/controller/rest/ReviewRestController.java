a8cc813860f12f26cafd672cf7d6c2a68290386c
Roberto <r.acebes.2024@alumnos.urjc.es>
2026-04-28 09:57:07 +0200

    change imports

--- src/main/java/es/codeurjc/ferrumgym/controller/rest/ReviewRestController.java
+++ src/main/java/es/codeurjc/ferrumgym/controller/rest/ReviewRestController.java
@@ -1,22 +1,15 @@
 package es.codeurjc.ferrumgym.controller.rest;
 
 import es.codeurjc.ferrumgym.dto.ReviewDTO;
-import es.codeurjc.ferrumgym.model.Review;
 import es.codeurjc.ferrumgym.service.ReviewService;
-import es.codeurjc.ferrumgym.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/api/v1/reviews")
