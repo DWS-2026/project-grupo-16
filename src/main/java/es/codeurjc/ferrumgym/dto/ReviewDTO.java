b616dc9bedf2e2058cb59c00793a9bcf14af31ff
Elena <e.gonzalezro.2024@alumnos.urjc.es>
2026-05-07 17:23:28 +0200

    change

--- src/main/java/es/codeurjc/ferrumgym/dto/ReviewDTO.java
+++ src/main/java/es/codeurjc/ferrumgym/dto/ReviewDTO.java
@@ -4,7 +4,7 @@ public record ReviewDTO(
     Long id,
     String comment,
     Integer rating,
-    String userName,     // Solo el nombre, no el objeto User entero
-    String activityName, // Solo el nombre, no el objeto Activity entero
+    String userName,     
+    String activityName, 
     String imageUrl
 ) {}
